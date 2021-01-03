29bb8016e7610854ab1861a799d3c68a48bbc6eb
Thirupathi Kola <thirupathikola@Thirupathis-MBP.home>
2021-01-02 23:03:18 -0500

    aa

--- First.js
+++ First.js
@@ -1,24 +1,54 @@
+String = "Thiru", '123Manu', '3242423', "123.43"
+Number = 1, 223.23, 645345
+Boolean = true, false
 
-calc => a
-    input 
-    input2
-
-
-
-    
-function calc(opt){
-    var a = document.getElementById('first').value;
-    var b = document.getElementById('second').value;
-    var out;
-    if(opt == 'add'){
-        out = parseFloat(a)+parseFloat(b);
-    }else{
-        out = parseFloat(a)-parseFloat(b);
-    }
-    return out;
-    //document.getElementById('output').innerText = out
-}
-
-
-var value = calc("add")
-
+var a;
+undefined
+var b;
+undefined
+a=10;
+10
+a
+10
+var b=100;
+undefined
+var b;
+undefined
+b
+100
+b=200;
+200
+b
+200
+let a;
+VM289:1 Uncaught SyntaxError: Identifier 'a' has already been declared
+var a;
+undefined
+a=18
+18
+a
+18
+const a;
+VM419:1 Uncaught SyntaxError: Missing initializer in const declaration
+a=30;
+30
+a
+30
+const z;
+VM496:1 Uncaught SyntaxError: Missing initializer in const declaration
+z=100
+100
+z
+100
+const l=10;
+undefined
+l
+10
+const l;
+VM683:1 Uncaught SyntaxError: Missing initializer in const declaration
+const l=100;
+VM721:1 Uncaught SyntaxError: Identifier 'l' has already been declared
+l=19;
+VM748:1 Uncaught TypeError: Assignment to constant variable.
+    at <anonymous>:1:2
+(anonymous) @ VM748:1
---
+++ index.js
@@ -0,0 +1,12 @@
+<html>
+<body>
+
+    <h2>First program JavaScript</h2>
+
+    <p id="demo">Hello this is me Javascript</p>
+
+    <button type="button" 
+    onclick='document.getElementById("demo").innerHTML="I am JavaScript"'> Click Me!</button>
+
+</body>
+</html>
